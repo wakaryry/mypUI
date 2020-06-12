79f5e2a70c744e50725c05717f2248b048b4c2b6
wakaryry <mayuping321@163.com>
2020-06-12 15:21:37 +0800

    新增myp-x-bar组件
    用于外部自行使用的安全区适配

---
+++ mypUI/myp-x-bar/myp-x-bar.vue
@@ -0,0 +1,33 @@
+<template>
+	<view :class="['myp-x-bar', 'myp-bg-'+bgType]" :style="xBarStyle"></view>
+</template>
+
+<script>
+	import windowMixin from '../myp-mixin/windowMixin.js'
+	
+	export default {
+		mixins: [windowMixin],
+		props: {
+			bgType: {
+				type: String,
+				default: ''
+			},
+			boxStyle: {
+				type: String,
+				default: ''
+			}
+		},
+		computed: {
+			xBarStyle() {
+				const h = this.mypGetXBarHeight()
+				return `height: ${h}px;` + this.boxStyle
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.myp-x-bar {
+		width: 750rpx;
+	}
+</style>

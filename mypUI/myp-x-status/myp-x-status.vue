414354fa449da1f9f129e94085220a3bde7b640c
wakaryry <mayuping321@163.com>
2020-06-17 12:35:23 +0800

    新增myp-x-status占位组件

---
+++ mypUI/myp-x-status/myp-x-status.vue
@@ -0,0 +1,30 @@
+<template>
+	<view :class="['myp-bg-'+bgType]" :style="xBarStyle"></view>
+</template>
+
+<script>
+	import windowMixin from '../myp-mixin/windowMixin.js'
+	
+	export default {
+		mixins: [windowMixin],
+		props: {
+			bgType: {
+				type: String,
+				default: ''
+			},
+			boxStyle: {
+				type: String,
+				default: ''
+			}
+		},
+		computed: {
+			xBarStyle() {
+				const h = this.mypGetStatusBarHeight()
+				return `height: ${h}px;` + this.boxStyle
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>

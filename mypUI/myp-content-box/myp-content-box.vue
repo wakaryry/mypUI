d7fb0efbf605461768d6c2b97ff1a9b94c0fe504
wakaryry <mayuping321@163.com>
2020-06-28 17:39:18 +0800

    up content box components

---
+++ mypUI/myp-content-box/myp-content-box.vue
@@ -0,0 +1,16 @@
+<template>
+	<view :class="['myp-bg-'+bgType]" :style="mrScrollStyle+boxStyle">
+		<slot></slot>
+	</view>
+</template>
+
+<script>
+	import styleMixin from '../myp-list/styleMixin.js'
+	
+	export default {
+		mixins: [styleMixin]
+	}
+</script>
+
+<style>
+</style>

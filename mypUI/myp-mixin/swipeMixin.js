28330f24e44dae8d7ef896624a4c484a2e083625
wakaryry <mayuping321@163.com>
2020-07-24 21:59:33 +0800

    增加swipe判断的mixin

---
+++ mypUI/myp-mixin/swipeMixin.js
@@ -0,0 +1,69 @@
+import touchMixin from './touchMixin.js'
+
+export default {
+	mixins: [touchMixin],
+	data() {
+		return {
+			mypSwipeMinTime: 60, // ms
+			mypSwipeMinOffset: 30, // px
+			mypSwipeStartPoint: null,
+			mypSwipeStartTime: 0
+		}
+	},
+	methods: {
+		mypSwipeTouchStart(e) {
+			this.mypSwipeStartTime = Date.now()
+			this.mypSwipeStartPoint = this.mypGetPoint(e)
+		},
+		mypSwipeTouchEnd(e) {
+			const end = this.mypGetPoint(e)
+			const endX = end.x
+			const endY = end.y
+			const time = Date.now() - this.mypSwipeStartTime
+			
+			if (time >=this.mypSwipeMinTime) {
+				const xOffset = endX - this.mypSwipeStartPoint.x
+				const yOffset = endY - this.mypSwipeStartPoint.y
+				let xDirection = ''  // x方向
+				let yDirection = ''  // y方向
+				let xyDirection = ''  // 取间距最大的那个
+				if (Math.abs(xOffset) >= this.mypSwipeMinOffset) {
+					if (xOffset < 0) {
+						xDirection = 'left'
+					} else {
+						xDirection = 'right'
+					}
+				}
+				if (Math.abs(yOffset) >= this.mypSwipeMinOffset) {
+					if (yOffset < 0) {
+						yDirection = 'up'
+					} else {
+						yDirection = 'down'
+					}
+				}
+				if (Math.abs(xOffset) >= Math.abs(yOffset)) {
+					xyDirection = xDirection
+				} else {
+					xyDirection = yDirection
+				}
+				return {
+					x: xDirection,
+					y: yDirection,
+					xy: xyDirection,
+					time: time
+				}
+			} else {
+				return {
+					x: '',
+					y: '',
+					xy: '',
+					time: time
+				}
+			}
+		},
+		mypSwipeTouchCancel(e) {
+			this.mypSwipeStartTime = 0
+			this.mypSwipeStartPoint = null
+		}
+	}
+}

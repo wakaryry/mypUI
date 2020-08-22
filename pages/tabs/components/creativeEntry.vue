038205e6878c0ea7a48b24348031ed23bd95bd37
wakaryry <mayuping321@163.com>
2020-08-22 22:45:09 +0800

    up mine

--- pages/tabs/components/creativeEntry.vue
+++ pages/tabs/components/creativeEntry.vue
@@ -1,10 +1,22 @@
 <template>
 	<view class="ce">
 		<myp-title title1="创作中心" title1Type="text" title1Size="l" title1Style="font-weight: 700;" boxStyle="padding-left: 24rpx;padding-right:24rpx;">
-			<myp-button bgType="error" border="none" radius="ll" icon="upload" text="发布" iconType="inverse" textType="inverse" boxStyle="width:160rpx;height:60rpx;"></myp-button>
+			<myp-button bgType="error" border="none" radius="ll" icon="upload" text="发布" iconType="inverse" textType="inverse" boxStyle="width:160rpx;height:60rpx;">
+				<myp-badge slot="extra" boxStyle="position:absolute;right:0;top:-6rpx;"></myp-badge>
+			</myp-button>
 		</myp-title>
 		<view class="ce-grid">
-			<myp-item v-for="(item,idx) in items" :key="idx" :icon="item.icon" :text="item.name" textType="text" iconType="warning" iconStyle="font-size: 60rpx;" space="20rpx" boxStyle="flex:1;height:180rpx;"></myp-item>
+			<!-- 只是示范，告诉你app/H5端可以这样 -->
+			<!-- #ifdef APP-NVUE || H5 -->
+			<myp-item v-for="(item,idx) in items" :key="idx" :icon="item.icon" :text="item.name" textType="text" iconType="warning" iconStyle="font-size: 60rpx;" space="20rpx" boxStyle="flex:1;height:180rpx;">
+				<myp-badge v-if="item.badge" :text="item.number" size="32rpx" boxStyle="position:absolute;right:46rpx;top:8rpx;"></myp-badge>
+			</myp-item>
+			<!-- #endif -->
+			<!-- #ifndef APP-NVUE || H5 -->
+			<myp-item v-for="(item,idx) in items" :key="idx" :icon="item.icon" :text="item.name" textType="text" iconType="warning" iconStyle="font-size: 60rpx;" space="20rpx" boxStyle="width:187rpx;height:180rpx;">
+				<myp-badge v-if="item.badge" :text="item.number" size="32rpx" boxStyle="position:absolute;right:46rpx;top:8rpx;"></myp-badge>
+			</myp-item>
+			<!-- #endif -->
 		</view>
 	</view>
 </template>
@@ -14,7 +26,7 @@
 		data() {
 			return {
 				items: [
-					{icon: 'report', name: '创作首页'},{icon: 'edu', name: '创作学院'},{icon: 'subject', name: '创作日历'},{icon: 'task', name: '热门活动'},
+					{icon: 'report', name: '创作首页'},{icon: 'edu', name: '创作学院'},{icon: 'subject', name: '创作日历'},{icon: 'task', name: '热门活动', badge: true, number: 1},
 				]
 			}
 		}

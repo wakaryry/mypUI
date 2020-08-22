8563e254b3ccefdd72a8183df5450b64bc5db91c
wakaryry <mayuping321@163.com>
2020-08-22 23:15:53 +0800

    up mine page

--- pages/tabs/components/moreEntry.vue
+++ pages/tabs/components/moreEntry.vue
@@ -1,8 +1,18 @@
 <template>
+	<view class="me">
+		<myp-title title1="更多服务" title1Type="text" title1Size="l" title1Style="font-weight: 700;" boxStyle="padding-left: 24rpx;padding-right:24rpx;"></myp-title>
+		<myp-cell icon="bell" title="联系客服" iconType="error" iconStyle="font-size:42rpx;" space="20rpx" indicator="right" boxStyle="padding-left: 48rpx;padding-right:32rpx;"></myp-cell>
+		<myp-cell icon="class" title="课堂模式" iconType="error" iconStyle="font-size:42rpx;" space="20rpx" indicator="right" boxStyle="padding-left: 48rpx;padding-right:32rpx;"></myp-cell>
+		<myp-cell icon="shen" title="青少年模式" iconType="error" iconStyle="font-size:42rpx;" space="20rpx" indicator="right" boxStyle="padding-left: 48rpx;padding-right:32rpx;"></myp-cell>
+		<myp-cell icon="setting" title="设置" iconType="error" iconStyle="font-size:42rpx;" space="20rpx" indicator="right" boxStyle="padding-left: 48rpx;padding-right:32rpx;"></myp-cell>
+	</view>
 </template>
 
 <script>
+	export default {
+		
+	}
 </script>
 
-<style>
+<style lang="scss" scoped>
 </style>
--- pages/tabs/components/recommendEntry.vue
+++ pages/tabs/components/recommendEntry.vue
@@ -1,8 +1,22 @@
 <template>
+	<view class="re">
+		<myp-title title1="推荐服务" title1Type="text" title1Size="l" title1Style="font-weight: 700;" boxStyle="padding-left: 24rpx;padding-right:24rpx;"></myp-title>
+		<myp-grid :items="items" :columns="4" iconLabel="icon" textLabel="name" iconType="error" iconTextSpace="20rpx" itemIconStyle="font-size:60rpx;" itemHeight="180rpx" :hasLine="false" border="none" radius="none"></myp-grid>
+	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				items: [{icon: 'book', name: '我的课程'},{icon: 'phone', name: '免流量服务'},{icon: 'love', name: '个性装扮'},{icon: 'users', name: '邀请赚钱'},{icon: 'search', name: '游戏中心'},{icon: 'deal', name: '我的钱包'},{icon: 'shop', name: '会员购中心'},{icon: 'circle-play', name: '直播中心'},{icon: 'comment-dots', name: '反馈论坛'},{icon: 'location', name: '充能领福利'}]
+			}
+		}
+	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.re {
+		
+	}
 </style>

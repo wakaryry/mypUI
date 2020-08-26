eff95c10b200ddc58b04ce4f0d336cab9d8ef1e4
wakaryry <mayuping321@163.com>
2020-08-26 14:40:24 +0800

    简化myp-input-one的配置，直接设置style

--- mypUI/myp-input-one/myp-input-one.vue
+++ mypUI/myp-input-one/myp-input-one.vue
@@ -1,19 +1,19 @@
 <template>
-	<view class="myp-one">
-		<view v-for="(item, index) in ranges" :key="index" :class="['myp-one-item', mode==='box'&&('myp-border-color-'+(type&&type.length>0?type:'text'))]" :style="mrItemStyle+(index!==ranges.length-1?('margin-right:'+space+';'):'')">
-			<view v-if="mode === 'middle' && codeIndex <= item" :class="['myp-bg-'+(type&&type.length>0?type:'text')]" :style="mrMiddleStyle"></view>
-			<view v-if="mode === 'bottom'" :class="['myp-bg-'+(type&&type.length>0?type:'text')]" :style="mrBottomStyle"></view>
-			<view v-if="secret && codeArr.length >= item" :class="['myp-bg-'+(type&&type.length>0?type:'text')]" :style="mrDotStyle"></view>
-			<text v-else :class="['myp-one-item-text', 'myp-color-'+type]" :style="mrTextStyle">{{ codeArr[index] ? codeArr[index] : ''}}</text>
+	<view class="myp-one" :style="boxStyle">
+		<view v-for="(item, index) in ranges" :key="index" class="myp-one-item" :style="mrItemStyle+(codeArr.length+1 === item?activeItemStyle:'')+(index!==ranges.length-1?('margin-right:'+space+';'):'')">
+			<view v-if="mode === 'middle' && codeIndex <= item" :style="mrMiddleStyle"></view>
+			<view v-if="mode === 'bottom'" :style="mrBottomStyle+(codeArr.length+1 === item?activeLineStyle:'')"></view>
+			<view v-if="mode==='box' && codeArr.length+1 === item" class="myp-one-cursor" :style="mrCursorStyle"></view>
+			<view v-if="secret && codeArr.length >= item" :style="mrDotStyle"></view>
+			<text v-else class="myp-one-item-text" :style="'line-height:'+width+';'+valueStyle">{{ codeArr[index] ? codeArr[index] : ''}}</text>
 		</view>
-		<input type="number" :value="inputValue" :focus="autoFocus" :maxlength="maxlength" class="myp-one-hide-input" :style="{height: width}" @input="getVal" />
+		<input type="number" :value="inputValue" :focus="focus" :maxlength="maxlength" class="myp-one-hide-input" :style="{height: width}" @input="getVal" />
 	</view>
 </template>
 
 <script>
 	// v-model or refs or nothing(just @finish)
 	export default {
-		name: 'mypOneInput',
 		props: {
 			value: {
 				type: String,
@@ -28,7 +28,11 @@
 				type: Boolean,
 				default: false
 			},
-			autoFocus: {
+			cursor: {
+				type: Boolean,
+				default: false
+			},
+			focus: {
 				type: Boolean,
 				default: false
 			},
@@ -36,19 +40,6 @@
 			mode: {
 				type: String,
 				default: "middle"
-			},
-			type: {
-				type: String,
-				default: ''
-			},
-			// 这里不使用scss中定义的size系列。直接赋值
-			size: {
-				type: String,
-				default: '70rpx'
-			},
-			weight: {
-				type: [String, Number],
-				default: '700'
 			},
 			// height/width相等，且不是scss中定义的系列。直接赋值
 			width: {
@@ -59,21 +50,37 @@
 				type: String,
 				default: '12rpx'
 			},
-			dotWidth: {
+			dotStyle: {
 				type: String,
-				default: '16rpx'
+				default: 'width:16rpx;height:16rpx;border-radius:16rpx;background-color:#000000;'
 			},
-			lineWidth: {
+			lineStyle: {
 				type: String,
-				default: '80rpx'
+				default: 'width:80rpx;height:8rpx;border-radius:12rpx;background-color:#000000;'
 			},
-			lineHeight: {
+			activeLineStyle: {
 				type: String,
-				default: '8rpx'
+				default: ''
 			},
-			boxRadius: {
+			cursorStyle: {
 				type: String,
-				default: '4rpx'
+				default: 'width:1px;height:42rpx;background-color:#8F9CFF;'
+			},
+			valueStyle: {
+				type: String,
+				default: 'font-size:70rpx;font-weight:700;color:#000000;'
+			},
+			boxStyle: {
+				type: String,
+				default: ''
+			},
+			itemStyle: {
+				type: String,
+				default: ''
+			},
+			activeItemStyle: {
+				type: String,
+				default: ''
 			}
 		},
 		watch: {
@@ -106,34 +113,41 @@
 			}
 		},
 		computed: {
+			widthUnit() {
+				if (this.width.indexOf('rpx') >= 0 || this.width.indexOf('upx') >= 0) {
+					return 'rpx'
+				}
+				return 'px'
+			},
 			mrItemStyle() {
 				let _style = `width:${this.width};height:${this.width};`
 				if (this.mode === 'box') {
-					_style += `border-radius:${this.boxRadius};border-width:1px;border-style:solid;`
+					_style += `border-radius:0;border-width:1px;border-style:solid;border-color:#000000;`
 				}
-				return _style
+				return _style + this.itemStyle
 			},
-			mrTextStyle() {
-				let _style = `font-size:${this.size};line-height:${this.width};weight:${this.weight};`
-				return _style
+			mrCursorStyle() {
+				const iw = parseInt(this.width)
+				const _style = `left:${iw*0.5}${this.widthUnit};top:${iw*0.5}${this.widthUnit};`
+				return _style + this.cursorStyle
 			},
 			mrMiddleStyle() {
 				const iw = parseInt(this.width)
-				let _style = `position:absolute;left:${iw*0.5}rpx;top:${iw*0.5}rpx;height:${this.lineHeight};width:${this.lineWidth};`
-				_style += `transform:translate(-50%,-50%);border-radius:${this.boxRadius};`
-				return _style
+				let _style = `position:absolute;left:${iw*0.5}${this.widthUnit};top:${iw*0.5}${this.widthUnit};`
+				_style += `transform:translate(-50%,-50%);`
+				return _style + this.lineStyle
 			},
 			mrBottomStyle() {
 				const iw = parseInt(this.width)
-				let _style = `position:absolute;left:${iw*0.5}rpx;bottom:0;height:${this.lineHeight};width:${this.lineWidth};border-radius:${this.boxRadius};`
+				let _style = `position:absolute;left:${iw*0.5}${this.widthUnit};bottom:0;`
 				_style += `transform:translateX(-50%);`
-				return _style
+				return _style + this.lineStyle
 			},
 			mrDotStyle() {
 				const iw = parseInt(this.width)
-				let _style = `position:absolute;left:${iw*0.5}rpx;top:${iw*0.5}rpx;height:${this.dotWidth};width:${this.dotWidth};border-radius:${this.dotWidth};`
+				let _style = `position:absolute;left:${iw*0.5}${this.widthUnit};top:${iw*0.5}${this.widthUnit};`
 				_style += `transform:translate(-50%,-50%);`
-				return _style
+				return _style + this.dotStyle
 			}
 		},
 		methods: {
@@ -173,7 +187,27 @@
 		flex-direction: row;
 		justify-content: center;
 		position: relative;
-
+		
+		&-cursor {
+			position: absolute;
+			transform: translate(-50%, -50%);
+			/* #ifndef APP-NVUE */
+			animation: shaking 1s infinite ease;
+			/* #endif */
+		}
+		/* #ifndef APP-NVUE */
+		@keyframes shaking {
+			0% {
+				opacity: 0.2;
+			}
+			50% {
+				opacity: 0.5;
+			}
+			100% {
+				opacity: 0.2;
+			}
+		}
+		/* #endif */
 		&-hide-input {
 			position: absolute;
 			top: 0;
@@ -183,7 +217,6 @@
 			opacity: 1;
 			background-color: transparent;
 		}
-
 		&-item {
 			position: relative;
 

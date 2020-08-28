c14e93bd093390bf7fed5bc46bd5f7e6608ceec5
wakaryry <mayuping321@163.com>
2020-08-28 15:14:09 +0800

    [+]myp-space

---
+++ mypUI/myp-space/myp-space.vue
@@ -0,0 +1,35 @@
+<template>
+	<view :class="['myp-bg-'+bgType, 'myp-border-'+border, 'myp-radius-'+radius, 'myp-space-'+size]" :style="boxStyle">
+		<slot></slot>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			size: {
+				type: String,
+				default: 'base'
+			},
+			bgType: {
+				type: String,
+				default: ''
+			},
+			border: {
+				type: String,
+				default: 'none'
+			},
+			radius: {
+				type: String,
+				default: 'none'
+			},
+			boxStyle: {
+				type: String,
+				default: ''
+			}
+		}
+	}
+</script>
+
+<style>
+</style>

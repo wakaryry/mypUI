0b08ab83e68c3c9d5692fbd969b0921b8a41bec9
wakaryry <mayuping321@163.com>
2020-09-01 11:12:14 +0800

    myp-list 适应文档自动生成

--- mypUI/myp-list/weexActions.js
+++ mypUI/myp-list/weexActions.js
@@ -1,55 +1,6 @@
 export default {
-	props: {
-		// 是否启用loading组件，而不是loadmoreofset触发
-		useLoading: {
-			type: Boolean,
-			default: false
-		},
-		// 使用loadmoreofset时的触发偏移量
-		loadMoreOffset: {
-			type: Number,
-			default: 60
-		}
-	},
-	data() {
-		return {
-			// down
-			mypDown: {
-				use: true
-			},
-			// up
-			mypUp: {
-				use: true
-			},
-			mypHasMore: true,
-			mypCurrentPage: 0,  // start with 0. if > 0, it means data inited
-			mypPrePage: 0,
-			mypIsDownLoading: false,
-			mypIsUpLoading: false,
-			platform: ''
-		}
-	},
-	created() {
-		// config the down/up
-		this.mypDown = Object.assign(this.down)
-		this.mypUp = Object.assign(this.up)
-		this.platform = this.mypGetPlatform()
-		// emit this 会在mp端报错，且不建议
-		// this.$emit("inited", this)
-		// 注意：如果直接emit，外部监听到inited的时候，还不能通过ref获取到实例
-		// this.$emit("inited")
-		setTimeout(()=>{
-			this.$emit("inited")
-		}, 0)
-		if (this.autoUpdate) {
-			const that = this
-			setTimeout(() => {
-				// to refresh data
-				this.mypInitContentList()
-			}, 10)
-		}
-	},
 	methods: {
+		// #ifdef APP-NVUE
 		mypResetLoadMore() {
 			this.$refs['myp-scroller'].resetLoadmore()
 		},
@@ -176,5 +127,6 @@ export default {
 				this.$emit('up')
 			}
 		}
+		// #endif
 	}
 }

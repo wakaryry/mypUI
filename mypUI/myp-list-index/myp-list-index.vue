946e3a256e5197a50b78b4d385beb09de563eb66
wakaryry <mayuping321@163.com>
2020-09-02 14:16:02 +0800

    简化 myp-list-index

--- mypUI/myp-list-index/myp-list-index.vue
+++ mypUI/myp-list-index/myp-list-index.vue
@@ -6,24 +6,19 @@
 				<slot name="head"></slot>
 			</cell>
 			<cell v-for="(v, i) in formatList" :key="i" :ref="'myp-index-title-' + (v.id||v.title)">
-				<view :class="[(v.type && v.type === 'group')?'myp-index-label-group':'myp-index-label', 'myp-bg-'+((v.type && v.type === 'group')?groupLabelBgType:labelBgType), 'myp-height-'+((v.type && v.type === 'group')?groupLabelHeight:labelHeight)]" :style="(v.type && v.type === 'group')?groupLabelStyle:labelStyle" v-if="!onlyShowList">
-					<text :class="['myp-color-'+((v.type && v.type === 'group')?groupLabelTextType:labelTextType), 'myp-size-'+((v.type && v.type === 'group')?groupLabelTextSize:labelTextSize)]" :style="(v.type && v.type === 'group')?groupLabelTextStyle:labelTextStyle">{{ v.title }}</text>
-				</view>
+				<text v-if="!onlyShowList" :class="['myp-index-title', v.type && v.type === 'group' && 'myp-index-title-group']" :style="(v.type && v.type === 'group')?groupTitleStyle:titleStyle">{{ v.title }}</text>
 				<view v-if="v.type && v.type === 'group' && !onlyShowList" class="myp-index-group">
 					<view v-for="(group, index) in v.data" :key="index" class="myp-index-group-list">
-						<view v-for="(item, i) in group" :key="i" class="myp-index-group-item" bubble="true" @click="itemClicked(item)">
-							<image v-if="item.isLocation" class="location-icon" src="https://gw.alicdn.com/tfs/TB1JUiUPFXXXXXUXXXXXXXXXXXX-32-32.png"></image>
-							<view>
-								<text class="myp-group-item-name">{{ item.name }}</text>
-								<text class="myp-group-item-desc" v-if="item.desc">{{ item.desc }}</text>
-							</view>
+						<view v-for="(item, i) in group" :key="i" class="myp-index-group-item" hover-class="myp-hover-opacity" :style="groupItemStyle" bubble="true" @click="itemClicked(item)">
+							<text class="myp-index-group-item-name" :style="groupItemTextStyle">{{ item.name }}</text>
+							<text class="myp-index-group-item-desc" :style="groupItemDescStyle" v-if="item.desc">{{ item.desc }}</text>
 						</view>
 					</view>
 				</view>
 				<view v-if="v.type === 'list'">
-					<view class="myp-index-item" v-for="(item, index) in v.data" :key="index" hover-class="myp-hover-opacity" @click="itemClicked(item)">
-						<text class="myp-index-item-title">{{ item.name }}</text>
-						<text class="myp-index-item-desc">{{ item.desc }}</text>
+					<view class="myp-index-item" v-for="(item, index) in v.data" :key="index" hover-class="myp-hover-opacity" :style="itemStyle" bubble="true" @click="itemClicked(item)">
+						<text class="myp-index-item-name" :style="itemTextStyle">{{ item.name }}</text>
+						<text class="myp-index-item-desc" :style="itemDescStyle">{{ item.desc }}</text>
 					</view>
 				</view>
 			</cell>
@@ -39,24 +34,21 @@
 		<scroll-view :scroll-y="true" :scroll-with-animation="true" :scroll-into-view="viewId" :class="'myp-bg-'+bgType" :style="mrScrollStyle" @scroll="toScroll">
 			<slot name="head"></slot>
 			<view v-for="(v, i) in formatList" :key="i" :id="'myp-index-title-' + (v.id||v.title)">
-				<text :class="['myp-index-title', v.type && v.type === 'group' && 'myp-group-title']" v-if="!onlyShowList">{{ v.title }}</text>
-				<view v-if="v.type && v.type === 'group' && !onlyShowList" class="myp-group">
-					<view v-for="(group, index) in v.data" :key="index" class="myp-group-list">
-						<view v-for="(item, i) in group" :key="i" @click="itemClicked(item)" class="myp-group-item">
-							<image v-if="item.isLocation" class="location-icon" src="https://gw.alicdn.com/tfs/TB1JUiUPFXXXXXUXXXXXXXXXXXX-32-32.png"></image>
-							<view>
-								<text class="myp-group-item-name">{{ item.name }}</text>
-								<text class="myp-group-item-desc" v-if="item.desc">{{ item.desc }}</text>
-							</view>
+				<text v-if="!onlyShowList" :class="['myp-index-title', v.type && v.type === 'group' && 'myp-index-title-group']" :style="(v.type && v.type === 'group')?groupTitleStyle:titleStyle">{{ v.title }}</text>
+				<view v-if="v.type && v.type === 'group' && !onlyShowList" class="myp-index-group">
+					<view v-for="(group, index) in v.data" :key="index" class="myp-index-group-list">
+						<view v-for="(item, i) in group" :key="i" class="myp-index-group-item" hover-class="myp-hover-opacity" :style="groupItemStyle" bubble="true" @click="itemClicked(item)">
+							<text class="myp-index-group-item-name" :style="groupItemTextStyle">{{ item.name }}</text>
+							<text class="myp-index-group-item-desc" :style="groupItemDescStyle" v-if="item.desc">{{ item.desc }}</text>
 						</view>
 					</view>
 				</view>
-				<view v-if="v.type === 'list'">
-					<view class="myp-index-item" v-for="(item, index) in v.data" :key="index" hover-class="myp-hover-opacity" @click="itemClicked(item)">
-						<text class="myp-index-item-title">{{ item.name }}</text>
-						<text class="myp-index-item-desc">{{ item.desc }}</text>
+				<block v-if="v.type === 'list'">
+					<view class="myp-index-item" v-for="(item, index) in v.data" :key="index" hover-class="myp-hover-opacity" :style="itemStyle" bubble="true" @click="itemClicked(item)">
+						<text class="myp-index-item-name" :style="itemTextStyle">{{ item.name }}</text>
+						<text class="myp-index-item-desc" :style="itemDescStyle">{{ item.desc }}</text>
 					</view>
-				</view>
+				</block>
 			</view>
 			<view :style="{height: footToken}"></view>
 			<view v-if="includeXBar&&overrideXBar" :style="mypXBarHeightStyle"></view>
@@ -69,7 +61,7 @@
 			<slot name="foot"></slot>
 		</view>
 		<view class="myp-index-nav" v-if="showIndex && !onlyShowList" :style="indexBoxStyle" bubble="true" @tap="toPrevent">
-			<text v-for="(item, index) in formatList" :key="index" :title="item.title" @tap.stop="go2Key(item.id||item.title, item.title)" class="myp-index-nav-key">{{ item.title }}</text>
+			<text v-for="(item, index) in formatList" :key="index" :title="item.title" @tap.stop="go2Key(item.id||item.title, item.title)" class="myp-index-nav-key" :style="indexTextStyle">{{ item.title }}</text>
 		</view>
 		<view class="myp-index-pop" v-if="popKeyShow">
 			<text class="myp-index-pop-text">{{ popKey }}</text>
@@ -78,10 +70,6 @@
 </template>
 
 <script>
-// indexlist比较简单，weex是dom.scrollToElement, uni直接scroll-into-view即可
-// 完全可以在页面里面自由使用mypUI的list组件，搭配完成各种list，比如可以刷新和加载更多，等等
-// 该indexlist只是为了最常用的indexlist的快速使用
-
 // #ifdef APP-NVUE
 const dom = weex.requireModule('dom');
 // #endif
@@ -108,56 +96,63 @@ export default {
 			type: Boolean,
 			default: true
 		},
-		hotListConfig: {
+		hotList: {
 			type: Object,
 			default: () => ({})
 		},
-		specialListConfig: {
+		specialList: {
 			type: Object,
 			default: () => ({})
-		},
-		groupHeight: {
-			type: String,
-			default: 's'
-		},
-		groupBgType: {
-			type: String,
-			default: 'page'
-		},
-		groupStyle: {
-			type: String,
-			default: ''
-		},
-		groupTitleType: {
-			type: String,
-			default: 'second'
-		},
-		groupTitleSize: {
-			type: String,
-			default: ''
 		},
 		groupTitleStyle: {
+			type: String,
+			default: ''
+		},
+		titleStyle: {
+			type: String,
+			default: ''
+		},
+		groupItemStyle: {
+			type: String,
+			default: ''
+		},
+		groupItemTextStyle: {
+			type: String,
+			default: ''
+		},
+		groupItemDescStyle: {
+			type: String,
+			default: ''
+		},
+		itemStyle: {
+			type: String,
+			default: ''
+		},
+		itemTextStyle: {
+			type: String,
+			default: ''
+		},
+		itemDescStyle: {
 			type: String,
 			default: ''
 		},
 		indexBoxStyle: {
 			type: String,
 			default: ''
+		},
+		indexTextStyle: {
+			type: String,
+			default: ''
+		},
+		boxStyle: {
+			type: String,
+			default: ''
 		}
 	},
 	computed: {
 		formatList() {
-			const { normalList, hotListConfig, specialListConfig } = this;
-			return Format.totalList(normalList, hotListConfig, specialListConfig);
-		},
-		heightPx() {
-			if (typeof this.height === 'number') {
-				return this.height
-			}
-			if (this.height.indexOf('rpx') >= 0) {
-				return uni.upx2px(parseInt(this.height))
-			}
-			return parseInt(this.height)
+			const { normalList, hotList, specialList } = this
+			return Format.totalList(normalList, hotList, specialList)
 		}
 	},
 	data: () => ({
@@ -176,7 +171,7 @@ export default {
 			this.$emit("scroll", e)
 		},
 		itemClicked(item) {
-			this.$emit('selected', item);
+			this.$emit('itemClicked', item);
 		},
 		go2Key(key, title) {
 			// #ifdef APP-NVUE
@@ -221,6 +216,47 @@ export default {
 		line-height: 48rpx;
 		padding-left: 32rpx;
 		width: 750rpx;
+		
+		&-group {
+			border-bottom-width: 0;
+			padding-bottom: 0;
+			height: 60rpx;
+			line-height: 60rpx;
+		}
+	}
+	&-group {
+		padding-bottom: 16rpx;
+		padding-right: 70rpx;
+		
+		&-list {
+			flex-direction: row;
+			margin-left: 18rpx;
+			margin-top: 18rpx;
+		}
+		&-item {
+			width: 146rpx;
+			height: 64rpx;
+			border-width: 1px;
+			border-color: $myp-border-color;
+			border-radius: 12rpx;
+			margin-right: 18rpx;
+			flex-direction: row;
+			align-items: center;
+			justify-content: center;
+			background-color: #ffffff;
+			
+			&-name {
+				font-size: 24rpx;
+				line-height: 26rpx;
+				color: $myp-text-color;
+			}
+			&-desc {
+				margin-top: 2rpx;
+				color: $myp-text-color-third;
+				font-size: 20rpx;
+				text-align: center;
+			}
+		}
 	}
 	&-item {
 		width: 750rpx;
@@ -232,7 +268,7 @@ export default {
 		background-color: #ffffff;
 		padding: 0 32rpx;
 		
-		&-title {
+		&-name {
 			font-size: 32rpx;
 			color: $myp-text-color;
 		}
@@ -284,56 +320,5 @@ export default {
 		left: 0;
 		bottom: 0;
 	}
-}
-
-.myp-group {
-	padding-bottom: 18rpx;
-	padding-right: 70rpx;
-	background-color: $myp-bg-color-page;
-	
-	&-title {
-		border-bottom-width: 0;
-		padding-bottom: 0;
-		height: 60rpx;
-		padding-top: 24rpx;
-	}
-	
-	&-list {
-		flex-direction: row;
-		margin-left: 18rpx;
-		margin-top: 18rpx;
-	}
-	
-	&-item {
-		width: 146rpx;
-		height: 64rpx;
-		border-width: 1px;
-		border-color: $myp-border-color;
-		border-radius: 12rpx;
-		margin-right: 18rpx;
-		flex-direction: row;
-		align-items: center;
-		justify-content: center;
-		background-color: #ffffff;
-		
-		&-name {
-			font-size: 24rpx;
-			line-height: 26rpx;
-			color: $myp-text-color;
-		}
-		
-		&-desc {
-			margin-top: 2rpx;
-			color: $myp-text-color-third;
-			font-size: 20rpx;
-			text-align: center;
-		}
-	}
-}
-
-.location-icon {
-	width: 32rpx;
-	height: 32rpx;
-	margin-right: 8rpx;
 }
 </style>

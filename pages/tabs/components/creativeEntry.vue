294971bc7425352e106810ea41a9b8b57b7ad989
wakaryry <mayuping321@163.com>
2020-09-02 09:27:12 +0800

    更换badge的位置

--- pages/tabs/components/creativeEntry.vue
+++ pages/tabs/components/creativeEntry.vue
@@ -1,9 +1,8 @@
 <template>
 	<view class="ce">
 		<myp-title title1="创作中心" title1Type="text" title1Size="l" title1Style="font-weight: 700;" boxStyle="padding-left: 24rpx;padding-right:24rpx;">
-			<myp-button bgType="error" border="none" radius="ll" icon="upload" text="发布" iconType="inverse" textType="inverse" boxStyle="width:160rpx;height:60rpx;">
-				<myp-badge slot="extra" boxStyle="position:absolute;right:0;top:-6rpx;"></myp-badge>
-			</myp-button>
+			<myp-button bgType="error" border="none" radius="ll" icon="upload" text="发布" iconType="inverse" textType="inverse" boxStyle="width:160rpx;height:60rpx;"></myp-button>
+			<myp-badge slot="extra" boxStyle="position:absolute;right:24rpx;top:12rpx;"></myp-badge>
 		</myp-title>
 		<view class="ce-grid">
 			<!-- 只是示范，告诉你app/H5端可以这样 -->

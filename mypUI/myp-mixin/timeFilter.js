5e2fc9af1e4b83953affa0db29ec3c9422a8bc06
wakaryry <mayuping321@163.com>
2020-09-11 23:51:27 +0800

    filter mixin 命名为 timeFilter

--- mypUI/myp-mixin/timeFilter.js
+++ mypUI/myp-mixin/timeFilter.js
@@ -1,6 +1,5 @@
 import {parseTime, formatTime} from '../utils/date.js'
 
-// since we can not use Vue.filter in nvue, we mix filters in every page
 export default {
 	filters: {
 		mypTimeParse: parseTime,

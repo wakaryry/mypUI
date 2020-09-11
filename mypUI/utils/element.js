f4e688ba4c3438798dd89d99888c73d04af1a467
wakaryry <mayuping321@163.com>
2020-09-11 23:51:45 +0800

    elMixin 转为 utils function

---
+++ mypUI/utils/element.js
@@ -0,0 +1,30 @@
+export function getElSize(name, ins) {
+	return new Promise((res, rej) => {
+		// #ifndef APP-NVUE
+		const el = uni.createSelectorQuery().in(ins).select('#' + name);
+		el.fields({
+			size: true,
+			rect: true
+		}, (data) => {
+			if (data) {
+				res(data);
+			} else {
+				rej({})
+			}
+		}).exec();
+		// #endif
+		// #ifdef APP-NVUE
+		let _el = ins.$refs[name][0]
+		if (!_el) {
+			_el = ins.$refs[name]
+		}
+		dom.getComponentRect(_el, (data) => {
+			if (data.result) {
+				res(data.size)
+			} else {
+				rej({})
+			}
+		})
+		// #endif
+	})
+}

c0d827d5aead179d9b108673d091288247e8a2a4
wakaryry <mayuping321@163.com>
2020-09-12 19:41:53 +0800

    getTouchPoint 改为 utils function

--- mypUI/utils/element.js
+++ mypUI/utils/element.js
@@ -28,3 +28,28 @@ export function getElSize(name, ins) {
 		// #endif
 	})
 }
+
+export function getTouchPoint(e) {
+	if (!e) {
+		return {
+			x: 0,
+			y: 0
+		}
+	}
+	if (e.touches && e.touches[0]) {
+		return {
+			x: e.touches[0].pageX,
+			y: e.touches[0].pageY
+		}
+	} else if (e.changedTouches && e.changedTouches[0]) {
+		return {
+			x: e.changedTouches[0].pageX,
+			y: e.changedTouches[0].pageY
+		}
+	} else {
+		return {
+			x: e.clientX,
+			y: e.clientY
+		}
+	}
+}

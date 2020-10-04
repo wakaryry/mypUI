f2bda69294709a53f2ab945d883f94a99dc0e033
wakaryry <mayuping321@163.com>
2020-10-04 19:14:16 +0800

    divider doc

---
+++ docsExtra/myp-divider.md
@@ -0,0 +1,18 @@
+
+其实就是 竖向的线 或者 横向的线。
+
+**示范**
+
+```html
+<myp-divider></myp-divider>
+
+<myp-divider left="32rpx" right="32rpx"></myp-divider>
+
+<myp-divider bgType="error" left="32rpx" right="32rpx"></myp-divider>
+
+<myp-divider bgType="error" :vertical="true" width="1px" height="80rpx" left="32rpx" right="32rpx"></myp-divider>
+```
+
+**结果**
+
+![myp-divider](/images/doc/divider.jpeg)
--- mypUI/myp-divider/myp-divider.vue
+++ mypUI/myp-divider/myp-divider.vue
@@ -7,38 +7,47 @@
 <script>
 	export default {
 		props: {
+			// 是否竖向
 			vertical: {
 				type: Boolean,
 				default: false
 			},
+			// 分割线背景主题
 			bgType: {
 				type: String,
 				default: 'border'
 			},
+			// 外层背景主题
 			boxBgType: {
 				type: String,
 				default: 'inverse'
 			},
+			// 外层背景色
 			boxBg: {
 				type: String,
 				default: ''
 			},
+			// 分割线背景色
 			bg: {
 				type: String,
 				default: ''
 			},
+			// 分割线左侧间距
 			left: {
 				type: String,
 				default: '0'
 			},
+			// 分割线右侧间距
 			right: {
 				type: String,
 				default: '0'
 			},
+			// 分割线宽度
 			width: {
 				type: String,
 				default: '1px'
 			},
+			// 分割线高度
 			height: {
 				type: String,
 				default: '1px'
